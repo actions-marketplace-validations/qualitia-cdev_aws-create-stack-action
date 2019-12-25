9826395876cc1679037aa9d01da1bf7cd01b13ef
cloudserviceuser <mihyon@qualita.co.jp>
2019-12-25 00:25:24 -0500

    Add arguments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Container image that runs your code
 FROM banst/awscli:latest
 
-# RUN apk add --no-cache python3 \
-#     && pip3 --no-cache-dir install awscli
-
 COPY create_stack /usr/local/bin
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,9 @@ echo "Start deploying"
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION=$3
+export AWS_ACCOUNT_NAME=$4
+readonly TARGET_DIR=$5
 
-echo ${AWS_ACCESS_KEY_ID}
-echo ${AWS_SECRET_ACCESS_KEY}
-echo ${AWS_DEFAULT_REGION}
-
-pwd
-env
-ls -a
+create_stack ${TARGET_DIR}
 
 echo "End deploy"
