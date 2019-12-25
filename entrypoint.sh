e7437bce81ae6c4323df6a832042f3052bc6a67c
Yasuhiro Ito <45684146+Yasuhiro0312@users.noreply.github.com>
2019-12-25 15:05:28 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,12 @@ export AWS_ACCOUNT_NAME=$4
 readonly TARGET_DIR=$5
 
 ls /usr/local/bin/ | grep create_stack
+
+ls -l /usr/local/bin
+
+chmod +x /usr/local/bin/create_stack
+cp /usr/local/bin/create_stak ./
+ls
 /usr/local/bin/create_stack ${TARGET_DIR}
 
 echo "End deploy"
