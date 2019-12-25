e44fe0e0bdef4539e3b4eb1e77933638cbbc7bab
Yasuhiro Ito <45684146+Yasuhiro0312@users.noreply.github.com>
2019-12-25 15:17:13 +0900

    Bugfix
    Since bash was not installed, create_stack could not be executed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,6 @@ export AWS_DEFAULT_REGION=$3
 export AWS_ACCOUNT_NAME=$4
 readonly TARGET_DIR=$5
 
-# ls /usr/local/bin/ | grep create_stack
-
-# ls -l /usr/local/bin
-
-# chmod +x /usr/local/bin/create_stack
-# cp /usr/local/bin/create_stak ./
-# ls
-# /usr/local/bin/create_stack ${TARGET_DIR}
-test.sh
+create_stack ${TARGET_DIR}
 
 echo "End deploy"
